4948553a7781ddd059c9f2214ad6f7db85f3e124
Nimer Farahty <nimer@nimer.mynet>
2017-08-04 14:58:23 +0300

    v 0.0.5

--- api/policies/checkPermission.js
+++ api/policies/checkPermission.js
@@ -11,15 +11,15 @@ module.exports = function (req, res, next) {
 
   User.findOne(req.user.id).populate('roles').exec(function (err, user) {
     if (err) {
-      return res.forbidden(err);
+      return res.status(403).send(err);
     } else if (!user) {
-      return res.forbidden(res.i18n('You Don\'t have permission to access this route'));
+      return res.status(403).send(res.i18n('You Don\'t have permission to access this route'));
     } else {
       Permission.findOne(where).populate('roles').exec(function (err, permission) {
         if (err) {
           return res.forbidden(err);
         } else if (!permission) {
-          return res.forbidden(res.i18n('You Don\'t have permission to access this route'));
+          return res.status(403).send(res.i18n('You Don\'t have permission to access this route'));
         } else {
           var userRoles = _.pluck(user.roles, 'id');
           var permissionRoles = _.pluck(permission.roles, 'id');
@@ -27,7 +27,7 @@ module.exports = function (req, res, next) {
           if (matches) {
             return next();
           } else {
-            return res.forbidden(res.i18n('You Don\'t have permission to access this route'));
+            return res.status(403).send(res.i18n('You Don\'t have permission to access this route'));
           }
         }
       });
