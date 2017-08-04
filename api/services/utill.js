874ee9b6209810bd94d91737e99f21cbe5e6277b
Nimer Farahty <nimer@nimer.mynet>
2017-08-04 14:02:29 +0300

    v 0.0.2

--- api/services/utill.js
+++ api/services/utill.js
@@ -9,6 +9,7 @@ module.exports = {
       iat: moment().unix(),
       exp: moment().add(1, 'h').unix()
     };
-    return jwt.encode(payload, sails.config.secret);
+    //return jwt.encode(payload, sails.config.secret);
+    return jwt.encode(payload, 'KGJGJHWDWDWXWcwcwc87w9c798w7c98bw78c97w9cwc');
   }
 }
